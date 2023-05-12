e05cc87063351c2559819f67f1a9f3ac69df017b
ryan2376 <ryannesh2376@gmail.com>
2023-05-12 09:32:52 +0300

    thursday

--- blackJack/index.js
+++ blackJack/index.js
@@ -40,18 +40,34 @@ let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+//let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-if (sum <= 20) {
-    console.log("do you want a new card?");
-} else if(sum === 21){
-    console.log("you win");
-    hasBlackJack = true
-}
-else{
-    console.log("you're out");
-    isAlive = false
+function startGame(){
+    renderGame()
 }
 
-
-console.log(hasBlackJack);
-console.log(isAlive);
+function renderGame(){
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "do you want a new card?"
+    } else if(sum === 21){
+        message = "you win";
+        hasBlackJack = true
+    }
+    else{
+        message = "you're out";
+        isAlive = false
+    } 
+   messageEl.textContent= message;
+}
+function newCard(){
+    console.log("Drawing new card from the deck");
+    let card = 7
+    sum += card
+    startGame()
+}
