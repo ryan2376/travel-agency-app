48e5fcad8f88f3aa6e5aea417b9dfa033215f567
ryan2376 <ryannesh2376@gmail.com>
2023-05-25 14:34:12 +0300

    pracThur

--- blackJack/loops.js
+++ blackJack/loops.js
@@ -57,11 +57,91 @@
 // }
 
 
-let castleListing = {
-    isSpacious : true,
-    cost : 350,
-    rooms : "twelve rooms",
-    address : [122, "londonstreet"]
-}
+// let castleListing = {
+//     isSpacious : true,
+//     cost : 350,
+//     rooms : "twelve rooms",
+//     address : [122, "londonstreet"]
+// }
 
-console.log(castleListing.isSpacious , castleListing.cost);
+// console.log(castleListing.isSpacious , castleListing.cost);
+
+
+// let person = {
+//     name: "Ryan",
+//     age:21,
+//     country: "Kenya"
+// }
+
+// function logData() {
+//    console.log(person.name + " is " + person.age + " years old and lives in " + person.country); 
+// }
+
+// logData()
+
+// let age = 70
+
+// if (age < 6) {
+//     console.log("free");
+// } else if (age < 17 || age === 6) {
+//     console.log("child discount");
+// } else if(age === 17 || age <= 26){
+//     console.log("student discount");
+// }else if(age === 26 || age <= 66){
+//     console.log("full price");
+// }else{
+//     console.log("senior citizen discount");
+// }    
+
+// let largeCountries = ["China","India","USA","Indonesia","Pakistan"]
+
+// function logCountries() {
+//     console.log("The 5 largest countries in the world:");
+//     for (let i = 0; i < largeCountries.length; i++) {
+//         console.log("-" + largeCountries[i])  
+//     }
+// }
+// logCountries()
+
+// let largeCountries =["Tuvalu","India","USA","Indonesia","Monaco"]
+
+// function removeSmall() {
+//     largeCountries.shift()
+//     console.log(largeCountries);
+//     largeCountries.pop()
+//     console.log(largeCountries);
+// }
+// removeSmall()
+
+// function addBig() {
+//     largeCountries.unshift("China")
+//     console.log(largeCountries);
+// }
+
+// largeCountries.push("Pakistan")
+// console.log(largeCountries);
+
+// addBig()
+
+// let dayOfMonth = 13
+// let weekDay = "Friday"
+
+// if (dayOfMonth === 13 && weekDay === "Friday") {
+//     console.log("😱");
+// } 
+
+// let hands = ["rock", "paper","scissors"]
+
+// function randomItem() {
+//      let randomIndex = Math.floor(Math.random()*3) 
+//      return hands[randomIndex]
+// }
+// console.log(randomItem()); 
+
+
+function sort() {
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+    }
+}
