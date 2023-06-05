3090b88a3de162dd91a4817252a8aa32e31bc6c8
ryan2376 <ryannesh2376@gmail.com>
2023-06-05 15:16:24 +0300

    template strings

--- blackJack/extensions.js
+++ blackJack/extensions.js
@@ -1,25 +1,41 @@
-let myLeads = ["bla","boo","bee"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const saveBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 saveBtn.addEventListener("click",function() {
-    console.log("Button clicked");
     myLeads.push(inputEl.value)
+    inputEl.value = "";
+    //window.open(myLeads[i],"_blank")
+    renderLeads()
 })
-
-
-for (let i = 0; i < myLeads.length; i++) {
-    //ulEl.innerHTML += "<li>" + myLeads[i] + "<li/>"
-    const li = document.createElement("li")
-    li.textContent = myLeads[i]
-    ulEl.append(li)
+function renderLeads() {
+    let listItems = ""
+    for (let i = 0; i < myLeads.length; i++) {
+        //listItems += "<li><a target ='_blank' href='" + myLeads[i] +"'>" + myLeads[i] + "</a><li/>"
+        //template strings
+        listItems += `<li>
+                          <a target ='_blank' href ='${myLeads[i]}'>
+                          ${myLeads[i]}
+                          </a>
+                      </li>
+                       `
 }
+ulEl.innerHTML = listItems
+}
+// //dom manipulation has a cost
 
+// // let boxEl = document.getElementById("box")
 
-// let boxEl = document.getElementById("box")
+// // boxEl.addEventListener("click", function() {
+// //     console.log("I want to open the box!");
+// // }) 
+// const recipient = "James"
+// let sender = 'Ryan'
 
-// boxEl.addEventListener("click", function() {
-//     console.log("I want to open the box!");
-// }) 
-
+// const email = `
+//                Hey ${recipient}! 
+//                How is it going?
+//                Cheers ${sender}
+//             `
+// console.log(email);
